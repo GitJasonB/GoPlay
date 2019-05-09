31a92726513d0b7994fc68aef1b7b07a763034df
Jason Robert Browner <jasonrbrown_uk@yahoo.com>
2019-05-09 17:01:18 +0100

    new

--- main.go
+++ main.go
@@ -7,6 +7,30 @@ import (
 func main() {
     fmt.Println("Hello World")
 
+    var i int 
+    var j int
+    j= 2
+    var myfloat float64
+    var o int = 1
+
+    fmt.Println(i) // prints out -127
+    fmt.Println(j) // prints out -127
+    fmt.Println(o) // prints out -127
+
+
+    var mynewint int
+    myfloat = float64(mynewint)
+
+    fmt.Println(myfloat) 
+
+    var myfloat3 float64
+myint2 := int(myfloat3)
+fmt.Println(myint2) // prints out -127
+
+
+
+
+
     var myint int8
     for i := 0; i < 129; i++ {
         myint += 1
@@ -14,4 +38,20 @@ func main() {
 
     }
   //  fmt.Println(myint) // prints out -127
+
+  var isTrue bool = true
+  var isFalse bool = false
+  // AND
+  if isTrue && isFalse {
+    fmt.Println("Both Conditions need to be True")
+  }
+  // OR - not exclusive
+  if isTrue || isFalse {
+    fmt.Println("Only one condition needs to be True")
+  }
+
+
+
+
+
 }
